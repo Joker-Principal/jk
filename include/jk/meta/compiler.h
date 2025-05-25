dd66b0d53ba2135cb41932d44b12fbe959cb58f1
Joker <joker2387089590@gmail.com>
2025-05-25 17:03:57 +0800

    [meta] compiler add __VA_OPT__ and macro tools

--- include/jk/meta/compiler.h
+++ include/jk/meta/compiler.h
@@ -3,6 +3,22 @@
 #define JK_STRINGIZE(x) #x
 #define JK_PRAGMA(x) _Pragma(JK_STRINGIZE(x))
 #define JK_CONCAT(x, y) x ## y
+#define JK_EXEC(f, a) f a
+#define JK_EMPTY(...)
+#define JK_COMMA(...) ,
+#define JK_EXPAND(x) x
+
+#define JK_SELECT0(_0, ...) _0
+#define JK_SELECT1(_0, _1, ...) _1
+#define JK_SELECT2(_0, _1, _2, ...) _2
+#define JK_SELECT3(_0, _1, _2, _3, ...) _3
+#define JK_SELECT4(_0, _1, _2, _3, _4, ...) _4
+#define JK_SELECT5(_0, _1, _2, _3, _4, _5, ...) _5
+#define JK_SELECT6(_0, _1, _2, _3, _4, _5, _6, ...) _6
+#define JK_SELECT7(_0, _1, _2, _3, _4, _5, _6, _7, ...) _7
+#define JK_SELECT8(_0, _1, _2, _3, _4, _5, _6, _7, _8, ...) _8
+#define JK_SELECT9(_0, _1, _2, _3, _4, _5, _6, _7, _8, _9, ...) _9
+#define JK_SELECT(i, ...) JK_SELECT##i(__VA_ARGS__)
 
 /// "overload" function macro by argument count
 // see also: https://stackoverflow.com/a/8814003
@@ -66,8 +82,39 @@
 JK_DISABLE_WARNING_PUSH
 JK_DISABLE_WARNING_UNUSED_MACRO
 
+// get count of __VA_ARGS__
+#if defined(JK_CLANG_CL) || (defined(JK_MSVC_ONLY) && (!defined(_MSVC_TRADITIONAL) || _MSVC_TRADITIONAL))
+#   define JK_X_ARGS_COUNT(...) unused, __VA_ARGS__
+#   define JK_Y_ARGS_COUNT(_1_, _2_, _3_, _4_, _5_, _6_, _7_, _8_, _9_, _10_, _11_, _12_, _13_, _14_, _15_, _16_, _17_, _18_, _19_, _20_, _21_, _22_, _23_, _24_, _25_, _26_, _27_, _28_, _29_, _30_, _31_, _32_, _33_, _34_, _35_, _36, _37, _38, _39, _40, _41, _42, _43, _44, _45, _46, _47, _48, _49, _50, _51, _52, _53, _54, _55, _56, _57, _58, _59, _60, _61, _62, _63, _64, _65, _66, _67, _68, _69, _70, count, ...) count
+#   define JK_Z_ARGS_COUNT(...) JK_EXPAND(JK_Y_ARGS_COUNT(__VA_ARGS__, 69, 68, 67, 66, 65, 64, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0))
+#   define JK_ARGS_COUNT(...)  JK_Z_ARGS_COUNT(JK_X_ARGS_COUNT(__VA_ARGS__))
+#else
+#   define JK_Y_ARGS_COUNT(_0, _1_, _2_, _3_, _4_, _5_, _6_, _7_, _8_, _9_, _10_, _11_, _12_, _13_, _14_, _15_, _16_, _17_, _18_, _19_, _20_, _21_, _22_, _23_, _24_, _25_, _26_, _27_, _28_, _29_, _30_, _31_, _32_, _33_, _34_, _35_, _36, _37, _38, _39, _40, _41, _42, _43, _44, _45, _46, _47, _48, _49, _50, _51, _52, _53, _54, _55, _56, _57, _58, _59, _60, _61, _62, _63, _64, _65, _66, _67, _68, _69, _70, count, ...) count
+#   define JK_ARGS_COUNT(...) JK_Y_ARGS_COUNT(0, ## __VA_ARGS__, 70, 69, 68, 67, 66, 65, 64, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
+#endif
+
+// check if __VA_OPT__ is supported
+// https://stackoverflow.com/a/48045656/21866441
+#define JK_Y_VA_OPT_SUPPORTED(...) JK_EXEC(JK_SELECT2, (__VA_OPT__(,), 1, 0))
+#define JK_VA_OPT_SUPPORTED JK_Y_VA_OPT_SUPPORTED(?)
+
+// emulate __VA_OPT__ for compilers that don't support it
+#if JK_VA_OPT_SUPPORTED
+#    define JK_VA_OPT(opt, ...) __VA_OPT__(opt)
+#    define JK_VA_OPT_FUNC(opt, ...) __VA_OPT__(opt)
+#    define JK_VA_OPT_COMMA(...) __VA_OPT__(,)
+#else
+#    define JK_X_OPT_IS_0() ,
+#    define JK_X_OPT_COUNT(count) JK_CONCAT(JK_X_OPT_IS_, count)
+#    define JK_X_OPT_SELECT(empty, opt, ...) (JK_X_OPT_COUNT(JK_ARGS_COUNT(__VA_ARGS__))(), empty, opt)
+#    define JK_VA_OPT(opt, ...) JK_EXEC(JK_SELECT2, JK_X_OPT_SELECT(, opt, __VA_ARGS__))
+#    define JK_VA_OPT_FUNC(opt, ...) JK_EXEC(JK_SELECT2, JK_X_OPT_SELECT(JK_EMPTY, opt, __VA_ARGS__))
+#    define JK_VA_OPT_COMMA(...) JK_EXEC(JK_SELECT2, JK_X_OPT_SELECT(JK_EMPTY, JK_COMMA,__VA_ARGS__))
+#endif
+
 /// unreachable
 #if defined(__cpp_lib_unreachable) // C++23
+#   include <utility>
 #   define JK_UNREACHABLE std::unreachable()
 #elif defined(JK_MSVC_ONLY)
 #   define JK_UNREACHABLE __assume(false)
